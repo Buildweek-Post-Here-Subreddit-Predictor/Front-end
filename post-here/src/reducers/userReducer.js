dd1746b2e7959c1d5a32cf76c8a62964d7d97835
Jess Daniel <jessdaniel474@gmail.com>
2019-11-17 21:32:15 -0500

    initialized CRA, setup redux store, and added dependencies

---
+++ post-here/src/reducers/userReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  credentials: {
+    username: "",
+    password: ""
+  }
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
