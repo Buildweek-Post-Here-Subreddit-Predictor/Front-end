51b3c3eb88b4fd24da0ba08f1c4faa2cab567a21
Jess Daniel <jessdaniel474@gmail.com>
2019-11-20 11:13:22 -0500

    Adding routes to app

--- post-here/src/App.js
+++ post-here/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+
+import LoginForm from "./components/LoginForm";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Route exact path="/" component={LoginForm} />
     </div>
   );
 }
